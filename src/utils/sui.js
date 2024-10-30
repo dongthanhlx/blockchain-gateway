81c94542daf1c8925670a8bc9c79cd0b9ea56a28
OneOne <dongthanhlx@gmail.com>
2024-10-30 14:28:53 +0700

    add decimal lib

--- src/utils/sui.js
+++ src/utils/sui.js
@@ -1,6 +1,7 @@
 const { Ed25519Keypair } = require('@mysten/sui/keypairs/ed25519');
 const { getFullnodeUrl, SuiClient } = require('@mysten/sui/client');
 const { Transaction } = require('@mysten/sui/transactions')
+const Decimal = require('decimal.js')
 
 function generateAddress() {
     const keyPair = new Ed25519Keypair();
@@ -13,12 +14,12 @@ function generateAddress() {
 
 async function buildTransaction({amount, fromKey, sendTo}) {
     const client = new SuiClient({
-        url: getFullnodeUrl('mainnet')
+        url: 'https://rpc.ankr.com/sui/0b0b99eee34ea3c2ab0132f62e87e8ed05d5ffec286f41d88ba87117fcea6e9d'
     })
     const tx = new Transaction();
     const keypair = Ed25519Keypair.fromSecretKey(fromKey)
 
-    const [coin] = tx.splitCoins(tx.gas, [amount * 1000000000]);
+    const [coin] = tx.splitCoins(tx.gas, [Decimal.mul(amount, 1000000000)]);
 
     tx.transferObjects([coin], sendTo);
 
@@ -26,7 +27,7 @@ async function buildTransaction({amount, fromKey, sendTo}) {
         signer: keypair, transaction: tx
     })
 
-    let {digest} = await client.waitForTransaction({ digest: result.digest });
+    let {digest, effects} = await client.waitForTransaction({ digest: result.digest, options: {showEffects: true} });
 
     return digest;
 }
